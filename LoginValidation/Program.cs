30f3569398d27225f9716918c279f8e2d9ee5deb
Opilane TTHK <opilane@tthk.ee>
2021-11-23 13:25:20 +0200

    Add project files.

---
+++ LoginValidation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "adming";
+            string password = "pass1234";
+
+            string userName, userPassword;
+
+            Console.WriteLine("Enter ur username: ");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("Enter ur password: ");
+            userPassword = Console.ReadLine();
+
+            if (login == userName && password == userPassword)
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Oops. Something went wrong.");
+            }
+
+        }
+    }
+}
