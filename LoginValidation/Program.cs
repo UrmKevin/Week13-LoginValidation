424c7c0e1dcde7cf3ca7d783b3d5c9dbf91d14ed
UrmKevin <93324340+UrmKevin@users.noreply.github.com>
2021-11-30 13:38:38 +0200

    Update Program.cs

--- LoginValidation/Program.cs
+++ LoginValidation/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LoginValidation
 {
@@ -8,22 +8,32 @@ namespace LoginValidation
         {
             string login = "adming";
             string password = "pass1234";
+            int attempts = 3;
 
             string userName, userPassword;
 
-            Console.WriteLine("Enter ur username: ");
-            userName = Console.ReadLine();
-
-            Console.WriteLine("Enter ur password: ");
-            userPassword = Console.ReadLine();
-
-            if (login == userName && password == userPassword)
+            while (attempts != 0)
             {
-                Console.WriteLine("Welcome!");
-            }
-            else
-            {
-                Console.WriteLine("Oops. Something went wrong.");
+                Console.WriteLine("Enter ur username: ");
+                userName = Console.ReadLine();
+                Console.WriteLine("Enter ur password: ");
+                userPassword = Console.ReadLine();
+
+                if (login != userName || password != userPassword)
+                {
+                    Console.WriteLine("Oops. Something went wrong.");
+                    attempts --;
+                    Console.WriteLine($"{3 - attempts} attempts left");
+                    if (attempts == 0)
+                    {
+                        Console.WriteLine("You don't have any attempts more");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Welcome!");
+                    break;
+                }
             }
 
         }
